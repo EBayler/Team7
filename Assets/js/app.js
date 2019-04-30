4fb2036b63df5d6f3e66fc4540974223701d3320
Allison <andersonaallison@gmail.com>
2019-04-29 22:04:50 -0600

    Cleaned up app.js, corrected API call for restaurants to correctly use the establishment ID instead of establishment keyword (pubs). :D

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,60 +1,46 @@
 $(document).ready(function () {
-
-
     $("#submit").on("click", function () {
+        var city = $("#searchForm").val();
+        getCities(city, processCities);
+    });
+});
 
+function getCities(city, resultCallback) {
+    $.ajax({
+        headers: {
+            "x-Zomato-API-Key": "29634845f26e2908bff359556e46203f"
+        },
+        url: "https://developers.zomato.com/api/v2.1/cities?q=" + $("#searchForm").val(),
+        success: resultCallback
+    });
+}
 
+function processCities(cityDatas) {
+    $("#cities").empty();
+    $("#restaurants").empty();
+    for (var i = 0; i < cityDatas.location_suggestions.length; i++) {
+        $("#cities").append("<input type='radio' id='city" + cityDatas.location_suggestions[i].name + "' name='city' value='" + i + "'>");
+        $("#cities").append("<label for='city" + cityDatas.location_suggestions[i].name + "'>" + cityDatas.location_suggestions[i].name + "</label><br/>");
+    }
+    $("#cities").append('<button id="select"  class="btn btn-outline-dark">Select</button>');
+    $("#searchForm").val("");
+    $("#select").on("click", function () {
+        var selectedCity = $("input:radio[name='city']:checked").val();
+        displayCity(cityDatas.location_suggestions[selectedCity].id);
+    });
+}
 
-        $.ajax({
-            headers: {
-                "x-Zomato-API-Key": "29634845f26e2908bff359556e46203f"
-            },
-            url: "https://developers.zomato.com/api/v2.1/cities?q=" + $("#searchForm").val(),
-
-            success: function (data) {
-                $("#cities").empty();
-                $("#restaurants").empty();
-                for (var i = 0; i < data.location_suggestions.length; i++) {
-                    $("#cities").append("<input type='radio' id='city" + data.location_suggestions[i].name + "' name='city' value='" + i + "'>");
-                    $("#cities").append("<label for='city" + data.location_suggestions[i].name + "'>" + data.location_suggestions[i].name + "</label><br/>");
-                }
-                $("#cities").append('<button id="select"  class="btn btn-outline-dark">Select</button>');
-                $("#searchForm").val("");
-
-
-                $("#select").on("click", function () {
-
-                    var selectedCity = $("input:radio[name='city']:checked").val()
-
-                    $.ajax({
-                        headers: {
-                            "x-Zomato-API-Key": "29634845f26e2908bff359556e46203f"
-                        },
-
-                        url: "https://developers.zomato.com/api/v2.1/search?entity_id=" + data.location_suggestions[selectedCity].id + "&entity_type=city&count=10&establishment_type=pub&sort=rating&order=desc",
-
-                        success: function (data) {
-                            $("#restaurants").empty();
-                            for (var i = 0; i < data.restaurants.length; i++) {
-                                $("#restaurants").append("<p>" + data.restaurants[i].restaurant.name + " with Rating " + data.restaurants[i].restaurant.user_rating.aggregate_rating + "</p>");
-                            }
-
-                        }
-                    });
-
-
-                })
-
-
-
-
+function displayCity(cityID) {
+    $.ajax({
+        headers: {
+            "x-Zomato-API-Key": "29634845f26e2908bff359556e46203f"
+        },
+        url: "https://developers.zomato.com/api/v2.1/search?entity_id=" + cityID + "&entity_type=city&count=10&establishment_type=7&sort=rating&order=desc",
+        success: function (searchDatas) {
+            $("#restaurants").empty();
+            for (var i = 0; i < searchDatas.restaurants.length; i++) {
+                $("#restaurants").append("<p>" + searchDatas.restaurants[i].restaurant.name + " with Rating " + searchDatas.restaurants[i].restaurant.user_rating.aggregate_rating + "</p>");
             }
-        });
-
-
-    })
-
-
-
-
-})
+        }
+    });
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Team7
+
+Git Workflow: 
+Everyone has their own working branch off of master.
+All changes must be pull requested into master.
+Everyone must pull master and then merge master into their branch.
