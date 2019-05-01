21a36bce1b5ed37d431f1869e6f2406461f7c9fb
Surin Cham <oSuRiNo@Surins-MacBook-Pro.local>
2019-04-30 18:23:13 -0600

    04.30.19

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-function inputValidation(){
-    var input = document.forms['searchForm'].value;
-    if (input == ''){
-        document.getElementById('modalMsg')
-    }
-    else{
+ var zip = input=(Number);
+ var city = input=(String)
 
-    }
-    if else{
+ // function to validate users input
+ // if blank, will use modal to have user search by either zip or city and state
+ // else num, will search by zip
+ // if  else, will search by city and state
+ function inputValidation() {
+     var input = document.forms['searchForm'].value;
+     if (input == city) {
+         console.log(city);
+     } else if (input == zip) {
+         console.log(zip);
+     } else {
+         console.log('Please search by:');
+     }
 
-    }
+ }
 
-}
-
-$(document).on("click", ".searchButton", function () {
-    console.log('fired');
-});
+ // Once submit button is fired
+ // will submit users input and search by option
+ $(document).on("click", ".searchButton", function(inputValidation) {
+     console.log('fired');
+ });
