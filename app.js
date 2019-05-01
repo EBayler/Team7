9419ab0bcfca6c3e40b71e1da66bce3751163a48
Surin Cham <oSuRiNo@Surins-MacBook-Pro.local>
2019-04-30 18:52:01 -0600

    04.30.19

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
- var zip = input=(Number);
- var city = input=(String)
+var city = input=(String)
 
  // function to validate users input
  // if blank, will use modal to have user search by either zip or city and state
  // else num, will search by zip
  // if  else, will search by city and state
- function inputValidation() {
-     var input = document.forms['searchForm'].value;
+ function inputValidation(event) {
+    event.preventDefault()
+    console.log('fired');
+     var input = document.forms('searchForm').value;
      if (input == city) {
          console.log(city);
-     } else if (input == zip) {
-         console.log(zip);
      } else {
-         console.log('Please search by:');
+         console.log('Please search by: city');
      }
 
  }
 
  // Once submit button is fired
  // will submit users input and search by option
- $(document).on("click", ".searchButton", function(inputValidation) {
-     console.log('fired');
- });
+ $(document).on("click", ".searchButton", inputValidation);
+
+
+//  Table
